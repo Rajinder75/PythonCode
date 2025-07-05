f1d4d38de5a4b4665b4d8089a4b9b1d1cadfe5c4
Rajinder Singh <rajindersingh22004@gmail.com>
2025-07-06 08:07:52 +1000

    Completed merge sort algorithm

--- merge_sort_algorithm.py
+++ merge_sort_algorithm.py
@@ -9,6 +9,11 @@ The algorithm works like this:
 """
 
 def merge_sort(array):
+
+    #In recursive solutions, a base case is required to stop the execution of the function
+    if len(array) <= 1:
+        return
+
     middle_point = len(array)//2
     left_part = array[:middle_point]
     right_part = array[middle_point:]
@@ -41,6 +46,32 @@ def merge_sort(array):
 
         # to add the next sorted element on the next index
         sorted_index +=1
+
+    #When the loop stops, there could be some elements still left in either left_part or right_part    
+    while left_array_index < len(left_part):
+        array[sorted_index] = left_part[left_array_index]
+        left_array_index +=1
+        sorted_index +=1
+
+    #for right_part
+    while right_array_index < len(right_part):
+        array[sorted_index] = right_part[right_array_index]
+        right_array_index += 1
+        sorted_index += 1
+
+#This checks if the python script is the main program or an imported module.
+#If this is not written, whenever the file is imported as a module, all the functions will be executed
+if __name__ == '__main__':
+    numbers = [4, 10, 6, 14, 2, 1, 8, 5]
+    print('Unsorted array: ')
+    print(numbers)
+    merge_sort(numbers)
+
+    print("Sorted array: "+str(numbers))
+    
+
+    
+        
 
     
 
