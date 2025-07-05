5336abc8f138faf172a8f9cdf277d0834c5a550e
Rajinder Singh <rajindersingh22004@gmail.com>
2025-07-05 22:07:06 +1000

    Added arithmetic arranger file

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,4 +1,5 @@
 def arithmetic_arranger(problems, show_answers=False):
+    space = " " 
     line_1=""
     line_2=""
     line_3=""
@@ -8,9 +9,8 @@ def arithmetic_arranger(problems, show_answers=False):
         for operation in problems:
             element_list = operation.split()
 
-            if element_list[1] != "+":
-                if element_list[1] != "-":
-                    return "Error: Operator must be '+' or '-'."
+            if element_list[1] not in ["+", "-"]:
+                return "Error: Operator must be '+' or '-'."
 
             elif (element_list[0].isdigit() == False) or (element_list[2].isdigit()==False):
                 return 'Error: Numbers must only contain digits.'
@@ -19,17 +19,21 @@ def arithmetic_arranger(problems, show_answers=False):
                 return 'Error: Numbers cannot be more than four digits.'
 
             else:
-                if len(element_list[0]) > (element_list[2]):
-                    line_1 += element_list[0] + " "*4
-                    line_2 += element_list[1] + " "*(element_list[])
+                greater = max(len(element_list[0]), len(element_list[2]))
+
+                number_of_dashes = greater + 2
+
+                line_1 += space*(number_of_dashes-len(element_list[0]))+element_list[0]+space*4
+                line_2 += element_list[1] + space*(number_of_dashes-len(element_list[2])-1) + element_list[2] + space*4
+                line_3 += "-"*number_of_dashes + space*4
+
+        arranged = line_1 + "\n" + line_2 + "\n" + line_3
+        
+      
+
+    return arranged
 
 
+arranged = arithmetic_arranger(['11 + 4', '3801 - 2999', '1 + 2', '123 + 49', '1 - 9380'])
+print(arranged)
 
-                #line_1 += element_list[0] + " "*4
-                #line_2 += element_list[1] + " " + element_list[2]
-                #line_3 += "_"*len(line_2)
-                
-        #problems = line_1 + "\n" + line_2 + "\n" +  line_3
-    #return problems
-
-#print(f'\n{arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"])}')
